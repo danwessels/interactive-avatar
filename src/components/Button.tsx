5536a734d44a39ebbe014a0167bbc21765d7ab8b
Danelle Wessels <dani.wessels@gmail.com>
2025-10-26 19:54:06 +0000

    Remove unused classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,5 @@
 const styles = {
   base: 'p-2 grow shadow-md max-w-17 border-2 rounded-xl transition-all duration-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-white/50 flex items-center justify-center',
-  blue: 'hover:text-blue-500 hover:border-blue-400 hover:bg-slate-900',
-  orange: 'hover:text-orange-500 hover:border-orange-400 hover:bg-slate-900',
-  orangeSelected:
-    'bg-linear-to-br from-orange-700 to-orange-400 border-orange-400 text-slate-900',
   purple: 'hover:text-purple-400 hover:border-purple-400 hover:bg-slate-900',
   purpleSelected:
     'bg-purple-400 border-purple-300 text-slate-900 inset-shadow-sm inset-shadow-purple-300',
